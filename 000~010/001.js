79a0a8fb03d04001ccbb2c5e9224516fc7552757
jskim95 <js2725@naver.com>
2020-05-23 16:34:25 +0900

    Create 001.js

---
+++ 000~010/001.js
@@ -0,0 +1,7 @@
+### 자바스크립트를 이용하여 문서에 HTML 요소를 추가하기
+
+document.createElement('div'); // <div></div>
+var divTest = document.createElement('div'); // <div></div>
+divTest.id = 'test'; // <div id="test"></div>
+
+
