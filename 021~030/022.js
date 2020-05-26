ea575ad40e9401fad8b25795bad26488d0294e57
jskim95 <js2725@naver.com>
2020-05-26 16:41:20 +0900

    Create 022.js

---
+++ 021~030/022.js
@@ -0,0 +1,11 @@
+비교 연산자
+// == 값비교
+// === 값비교 + 자료형 비교
+
+ex)
+    console.log(7 == 7) // true
+    console.log(7 == "7") // true
+    console.log(7 === "7") // false
+    console.log(7 === 7) // true
+    console.log(7 != "7") // false
+    console.log(7 !== "7") /true
