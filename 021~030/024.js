15bc7d5243a8ae13f7a2031e50449eb904fd6f53
jskim95 <js2725@naver.com>
2020-05-26 17:27:33 +0900

    Create 024.js

---
+++ 021~030/024.js
@@ -0,0 +1,13 @@
+삼항 연산자
+
+형식
+조건문 ? 표현문1 : 표현문2
+
+// true => 표현문1 실행
+// false => 표현문2 실행
+
+ex)
+    var a = true
+    var b = false
+    a ? console.log('left') : console.log('right') // left
+    b ? console.log('left') : console.log('right') // right
