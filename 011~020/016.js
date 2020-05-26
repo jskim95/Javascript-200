a0ca8672eb4b7d056f9e1fdd37cbdbf98c9b2d0e
jskim95 <js2725@naver.com>
2020-05-26 11:36:31 +0900

    Create 016.js

---
+++ 011~020/016.js
@@ -0,0 +1,21 @@
+숫자형
+// 자바스크립트의 숫자형은 정수, 실수, 작은 수, 큰 수, 모두 숫자형 하나로 정의함
+
+Infinity
+// 무한대 숫자(가장 큰 수)
+
+NAN
+// 유효하지 않은 값 OR 표현할 수 없는 너무 큰 수
+
+ex)
+    console.log(Infinity)
+    console.log(123123123/Infinity) 
+    console.log(0/0)
+    console.log("hi"/0)
+    
+    // 출력
+    
+    // Infinity
+    // 0
+    // NaN
+    // NaN
