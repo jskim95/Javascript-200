8b780dd9b203e6fb564a71027396c26904218a45
jskim95 <js2725@naver.com>
2020-05-26 12:27:04 +0900

    Create 019.js

---
+++ 011~020/019.js
@@ -0,0 +1,16 @@
+null
+// 변수 선언과 값을 초기화 하지만 값으로써 의미없는 특별한 값이 등록되어 있는 것
+
+undefined
+// 변수가 선언만 되고 값이 초기화 되지 않은 것
+
+ex)
+
+    var a = null;
+    var b;
+    
+    console.log(a) // null
+    console.log(typeof a) // object
+    
+    console.log(b) // undefined
+    console.log(typeof b) // undefined
